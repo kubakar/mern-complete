cb9b219c40ce8fd52f15cc88058a80f7eb72f86c
Jakub Karas <jakub.karas@motorolasolutions.com>
2024-02-09 14:57:19 +0100

    add connectDB info

--- src/dbConnect.ts
+++ src/dbConnect.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // returns promise
 const connectDB = () => {
   const password = process.env.MONGO_PASSWORD;
-  const dbName = "JOBIFY";
+  const dbName = "JOBIFY"; // base project
 
   const connectionString = `mongodb+srv://horneja:${password}@nodecluster.ssidncj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
   return mongoose.connect(connectionString);
